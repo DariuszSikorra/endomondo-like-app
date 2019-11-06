892ae06673c80e8e4f768e7c0b703d0372dca32a
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-11-06 17:25:33 +0100

    Updates

---
+++ precache-manifest.3e98377d3b8b5cf1badcb5371a7d5943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509b76f02932bd8aada9e2568a0fddc5",
+    "url": "/endomondo-like-app/index.html"
+  },
+  {
+    "revision": "0cd4b92591e092eb01b8",
+    "url": "/endomondo-like-app/static/css/main.515b67ea.chunk.css"
+  },
+  {
+    "revision": "556701f8a1b6fef9f0de",
+    "url": "/endomondo-like-app/static/js/2.6f076f2f.chunk.js"
+  },
+  {
+    "revision": "0cd4b92591e092eb01b8",
+    "url": "/endomondo-like-app/static/js/main.34e6010a.chunk.js"
+  },
+  {
+    "revision": "5b1c0c4d3317d3209e40",
+    "url": "/endomondo-like-app/static/js/runtime-main.57b6efac.js"
+  }
+]);
