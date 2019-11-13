2a4afb47f9f526b12d16306134b558d3ea834c80
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-11-13 14:59:11 +0100

    Updates

--- precache-manifest.be9944b62add69a7ba4e0ab61cd2f7da.js
+++ precache-manifest.be9944b62add69a7ba4e0ab61cd2f7da.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "778e3027dd5c0fb469e99c0a70964a6d",
+    "revision": "e8b026ebfa9dd49b384b21cd3cc330df",
     "url": "/endomondo-like-app/index.html"
   },
   {
-    "revision": "3a62f9b3d7f08b2c7dac",
+    "revision": "6f364639683903bd2a56",
     "url": "/endomondo-like-app/static/css/main.bb3ed7ad.chunk.css"
   },
   {
-    "revision": "6a2a365be4d5f9251679",
-    "url": "/endomondo-like-app/static/js/2.0a6ffdba.chunk.js"
+    "revision": "77811e3bf402be5d578b",
+    "url": "/endomondo-like-app/static/js/2.6935ec26.chunk.js"
   },
   {
-    "revision": "3a62f9b3d7f08b2c7dac",
-    "url": "/endomondo-like-app/static/js/main.2990db74.chunk.js"
+    "revision": "6f364639683903bd2a56",
+    "url": "/endomondo-like-app/static/js/main.31316948.chunk.js"
   },
   {
     "revision": "5b1c0c4d3317d3209e40",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/endomondo-like-app/precache-manifest.d5bae73552c00617da76e8ed1c7567e5.js"
+  "/endomondo-like-app/precache-manifest.be9944b62add69a7ba4e0ab61cd2f7da.js"
 );
 
 self.addEventListener('message', (event) => {
